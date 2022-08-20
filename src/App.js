fa167450fd01203c5aff270417022aa2f7b11922
Noelle Huchette <noelle.huchette@gmail.com>
2022-08-20 15:08:16 -0400

    moving styling

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Footer from './client/components/Footer/index';
 function App() {
   return (
     <div className="App">
-      <nav>
-        <Nav />
-      </nav>
+      <Nav />
       <main>
         <Routes />
       </main>
