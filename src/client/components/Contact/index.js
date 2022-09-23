c984b2a76ddfe54c5061c42977ee8ddc70889443
Noelle Huchette <noelle.huchette@gmail.com>
2022-09-23 17:37:13 -0400

    fixed footer, other layout fixes

---
+++ src/client/components/Contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Contact = () => {
+  return (
+    <div className="contact-page">
+      <h3>Contact Info</h3>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/client/components/Projects/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Projects = () => {
+  return (
+    <div className="projects-page">
+      <h3>Project Info</h3>
+    </div>
+  );
+};
+
+export default Projects;
