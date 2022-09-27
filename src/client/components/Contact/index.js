6da00f226295dcb6ab161f7ef32b82050938b364
Noelle Huchette <noelle.huchette@gmail.com>
2022-09-27 16:10:51 -0400

    simple styling on contact/project pages

--- src/client/components/Contact/index.js
+++ src/client/components/Contact/index.js
@@ -10,12 +10,14 @@ const Contact = () => {
         </div>
         <div className="contact-item">
           <a href="tel:8047620267" className="contact-label">
-            Phone: <span className="info">(804)762-0267</span>
+            <h4>Phone: </h4>
+            <span className="info">(804)762-0267</span>
           </a>
         </div>
         <div className="contact-item">
           <a className="contact-label" href="mailto:noelle.huchette@gmail.com">
-            Email: <span className="info">noelle.huchette@gmail.com</span>
+            <h4>Email: </h4>
+            <span className="info">noelle.huchette@gmail.com</span>
           </a>
         </div>
         <div className="contact-item">
@@ -23,7 +25,7 @@ const Contact = () => {
             className="contact-label"
             href="https://www.linkedin.com/in/noelle-huchette/"
           >
-            LinkedIn
+            <h4>LinkedIn</h4>
           </a>
         </div>
       </div>
