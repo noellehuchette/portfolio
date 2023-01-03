b492f16a82b21ccb992f47117332e6a834b8f937
Noelle Huchette <noelle.huchette@gmail.com>
2023-01-03 01:52:09 -0500

    hide carousel for debug

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Carousel from '../Elements/Carousel';
-import Slide from '../Elements/Carousel/Slide';
+// import Carousel from '../Elements/Carousel';
+// import Slide from '../Elements/Carousel/Slide';
 
 import './style.css';
 
